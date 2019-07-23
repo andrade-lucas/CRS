007302a1fee9e2e056773a248dd1bd266607d0b5
cheeky-lord <lucasilva108@gmail.com>
2019-07-22 22:13:35 -0300

    loginPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
